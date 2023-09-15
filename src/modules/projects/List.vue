5bcaf332c0bec09c0a2f196a82edffbd864a0df4
RobertoACJr <jr.ayala2001@gmail.com>
2023-09-15 19:24:57 -0400

    fix: adjusting visualization

--- src/modules/projects/List.vue
+++ src/modules/projects/List.vue
@@ -1,14 +1,65 @@
 <template>
   <v-container>
-    <v-row>
+    <div
+      class="text-h3 heading-3 mb-5"
+    >
+      Lista de Projetos
+    </div>
+    <loading v-if="loading" />
+    <v-row v-else-if="getIsAdmin">
+      <v-col :md="12">
+        <v-table
+          fixed-header
+          height="500px"
+        >
+          <thead>
+            <tr>
+              <th class="text-left">
+                Código
+              </th>
+              <th class="text-left">
+                Nome do Projeto
+              </th>
+              <th class="text-left">
+                Quantidade de Avaliações
+              </th>
+              <th class="text-left">
+                Média
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(project, index) in projects"
+              :key="index"
+              @click="goToProject(project)"
+            >
+              <td>{{ project.code }}</td>
+              <td>{{ project.name }}</td>
+              <td>{{ project.qtd_evaluations || "não avaliado" }}</td>
+              <td>{{ project.average || "não avaliado" }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+      <v-col :md="12">
+        <v-pagination
+          :value="currentPage"
+          :length="lenghtOfPages"
+          rounded="circle"
+          @update:model-value="getProjectsByPage"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-else>
       <v-col
-        v-for="(event, key) in projects"
+        v-for="(project, key) in projects"
         :key="key"
         md="4"
       >
         <CardProject
-          :event-title="event.title"
-          @click="goToProject(event)"
+          :project-title="project.name"
+          @click="goToProject(project)"
         />
       </v-col>
     </v-row>
@@ -17,7 +68,7 @@
 
 <script>
 import { defineComponent } from 'vue';
-import { mapMutations } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import CardProject from '@/modules/projects/components/CardProject.vue';
 
 export default defineComponent({
@@ -26,22 +77,58 @@ export default defineComponent({
     CardProject,
   },
   data: () => ({
+    loading: false,
     projects: [
-      { title: 'primeiro prtojeto' },
-      { title: 'segundo prtojeto' },
-      { title: 'terceiro prtojeto' },
-      { title: 'quarto prtojeto' },
-      { title: 'quinto prtojeto' },
+      { code: 'abs', qtd_evaluations: 2, name: 'primeiro projeto', average: 5.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'segundo projeto', average: 3.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'terceiro projeto', average: 8.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'quarto projeto', average: null },
+      { code: 'abs', qtd_evaluations: 2, name: 'quinto projeto', average: 9.6 },
+      { code: 'abs', qtd_evaluations: 2, name: 'primeiro projeto', average: 5.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'segundo projeto', average: 3.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'terceiro projeto', average: 8.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'quarto projeto', average: null },
+      { code: 'abs', qtd_evaluations: 2, name: 'quinto projeto', average: 9.6 },
+      { code: 'abs', qtd_evaluations: 2, name: 'primeiro projeto', average: 5.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'segundo projeto', average: 3.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'terceiro projeto', average: 8.5 },
+      { code: 'abs', qtd_evaluations: 2, name: 'quarto projeto', average: null },
+      { code: 'abs', qtd_evaluations: 2, name: 'quinto projeto', average: 9.6 },
     ],
+    currentPage: 1,
+    lenghtOfPages: 4,
   }),
+  computed: {
+    ...mapGetters('permissions', [
+      'getIsAdmin',
+    ])
+  },
   methods: {
     ...mapMutations('projects', [
       'setCurrentProject',
     ]),
-    goToProject(event) {
+    goToProject (event) {
       this.setCurrentProject(event);
       this.$router.push({ name: 'showProject' });
+    },
+    getProjectsByPage (page) {
+      if (page == this.currentPage) return;
+      this.currentPage = page;
+      // TODO get projects
     },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+td, th {
+  color: rgb(var(--v-theme-primary));
+}
+td {
+  cursor: pointer;
+}
+tr:hover {
+  background-color: rgb(var(--v-theme-light_primary));
+}
+
+</style>
--- src/modules/projects/Rate.vue
+++ src/modules/projects/Rate.vue
@@ -1,65 +1,122 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col
-        v-for="(assessment, index) in assessments"
-        :key="index"
-        xl="6"
-        md="12"
-      >
-        {{ assessment.title }}
-        <v-text-field 
-          v-model="assessment.score" 
-          height="5" 
-          :rules="rules" 
-          editable 
-          persistent-hint 
-          outline 
-          label="" 
-          type="number"
-        />
-      </v-col>
-    </v-row>
+    <div
+      class="text-h3 heading-3 mb-5"
+    >
+      {{ getCurrentProject.name }}
+    </div>
+    <v-form
+      v-model="isFormValid"
+      @submit.prevent="sendForm"
+    >
+      <v-row>
+        <v-col
+          v-for="(criterion, i) in criteria"
+          :key="i"
+          xl="6"
+          md="12"
+        >
+          <div
+            class="text-h5 heading-5 mb-3"
+          >
+            {{ criterion.title }}
+          </div>
+          <v-row>
+            <v-col
+              v-for="(evaluation, j) in criterion.sub_criteria"
+              :key="j"
+              md="6"
+            >
+              {{ evaluation.question }}{{ evaluation.required ? " *" : "" }}
+              <v-text-field
+                :value="evaluation.score"
+                label=""
+                height="5"
+                type="number"
+                editable
+                persistent-hint
+                outline
+                :rules="[
+                  v => evaluation.required && !v ? 'O campo é obrigatório' : true,
+                  v => v > 10 ? 'A nota não pode ser maior que 10' : true,
+                  v => v < 0 ? 'A nota não pode ser menor que 0' : true,
+                ]"
+                @update:model-value="(d) => evaluation.score = `${d}`.replace(',', '.')"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col :md="12">
+          <v-btn
+            type="submit"
+            block
+            class="mt-8"
+            color="primary"
+            size="large"
+            variant="tonal"
+          >
+            Avaliar Projeto
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: 'RateProject',
   data: () => ({
     requiredMessage: "O campo é obrigatório",
+    isFormValid: false,
     loading: false,
-    rules: [
-      v => v ? true : 'O campo é obrigatório',
-      v => v > 10 ? 'A nota não pode ser maior que 10' : true,
-      v => v < 0 ? 'A nota não pode ser menor que 0' : true,
-    ],
-    assessments: [
+    criteria: [
       {
-        title: 'O projeto é um projeto?',
-        uuid: 'uuuu-uuuu-iiii-dddd',
+        title: 'projeto',
+        sub_criteria: [
+          {
+            id: 1,
+            question: 'O projeto é um projeto?',
+            required: true,
 
-        score: null,
+            score: null,
+          }
+        ]
       },
       {
-        title: 'O projeto é tem um banner?',
-        uuid: 'uuuu-uuuu-iiii-dddd',
+        title: 'em caminho de paka...',
+        sub_criteria: [
+          {
+            id: 2,
+            question: 'o tatu caminha dentro?',
+            required: true,
 
-        score: null,
-      },
-      {
-        title: 'O projeto é um bonito igual o palma?',
-        uuid: 'uuuu-uuuu-iiii-dddd',
+            score: null,
+          },
+          {
+            question: 'se o caminho é..sim',
+            id: 3,
+            required: true,
 
-        score: null,
+          },
+        ]
       },
     ],
   }),
 
   computed: {
+    ...mapGetters('projects', [
+      'getCurrentProject',
+    ])
   },
 
   methods: {
+    sendForm() {
+      debugger;
+      if (!this.isFormValid) return;
+      debugger;
+    },
   },
 }
 </script>
