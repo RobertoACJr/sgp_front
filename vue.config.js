fd8164b9af1e247d0a7fcc789d9f9292379d0126
robertocostabefore <roberto.costa@before.com.br>
2025-10-01 19:33:43 -0400

    GAMBIARRA: para remover error-overlay

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,37 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 16900,
+    https: false,
+    hot: true,
+    allowedHosts: ['ifmscx.pro.br', 'localhost'],
+    client: {
+      overlay: {
+        warnings: false,
+        errors: false,
+      },
+    },
+  },
+
   configureWebpack: {
     entry: "./src/main.js",
-    devServer: {
-      open: process.platform === 'darwin',
-      host: '0.0.0.0',
-      port: 16900,
-      https: false,
-      hot: true,
-      allowedHosts: ['ifmscx.pro.br', 'localhost']
-    },
     watch: true,
     watchOptions: {
       ignored: /node_modules/,
       poll: 1000,
     },
+    plugins: [
+      new (require('webpack')).DefinePlugin({
+        'process.env.OVERLAY': JSON.stringify(false)
+      }),
+    ],
   },
+
   transpileDependencies: true,
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
