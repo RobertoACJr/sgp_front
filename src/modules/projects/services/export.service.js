8e6f39f703c1e489a012032f3141c40de4716999
robertocostabefore <roberto.costa@before.com.br>
2025-10-01 19:34:06 -0400

    FEAT: adicionando btn para exportar informações para avaliação

--- src/modules/projects/services/export.service.js
+++ src/modules/projects/services/export.service.js
@@ -1,6 +1,20 @@
 import * as exportApi from '@/modules/projects/api/export.js';
 import { errorToast } from '@/modules/core/helpers/toastHelper';
 
+export const exportProjectsBasicInformation = async () => {
+  try {
+    const { data } = await exportApi.exportProjectsBasicInformation();
+    return data;
+  } catch (error) {
+    console.error(error);
+    errorToast({
+      title: "Falha ao exportar informações dos projetos 😿",
+      text: error.response.data.message || "",
+    })
+    throw error;
+  }
+}
+
 export const exportProjectsRanking = async () => {
   try {
     const { data } = await exportApi.exportProjectsRanking();
@@ -8,7 +22,7 @@ export const exportProjectsRanking = async () => {
   } catch (error) {
     console.error(error);
     errorToast({
-      title: "Falha ao exportar Projetos",
+      title: "Falha ao exportar Projetos 😿",
       text: error.response.data.message || "",
     })
     throw error;
@@ -22,7 +36,7 @@ export const exportEvaluators = async () => {
   } catch (error) {
     console.error(error);
     errorToast({
-      title: "Falha ao exportar as informações dos avaliadores",
+      title: "Falha ao exportar as informações dos avaliadores 😿",
       text: error.response.data.message || "",
     })
     throw error;
